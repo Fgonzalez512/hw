9917642c4fb73e07805edb9d07467dd162d49000
Frank Gonzalez <frank.gonzalez628@gmail.com>
2017-02-07 12:38:10 -0600

    possibly changing color scheme

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,38 @@
 angular
     .module('webapp', ['ngMaterial', 'ngMessages', 'ngResource'])
-    .controller('DemoCtrl', function($scope, $http, $resource) {
+    .controller('DemoCtrl', function($scope, $http, $resource, $mdDialog) {
+
+        $scope.view = [];
+        // $scope.test = "booyah" booyah test to ensure angular info is passing through
+
+        $http.get('https://api.punkapi.com/v2/beers').then(res => {
+            // console.log(res.data);
+            $scope.view = res.data;
+        });
+
+        $scope.showDialog = function(e, item) {
+            $mdDialog.show({
+                locals: {
+                    item: item
+                },
+                controller: PopupCtrl,
+                controllerAs: 'dialog',
+                templateUrl: '/scripts/views/info.html',
+                parent: angular.element(document.body),
+                targetEvent: e,
+                clickOutsideToClose: true,
+            })
+        }
+
+        function PopupCtrl($scope, $mdDialog, item) {
+            $scope.item = item;
+
+            $scope.closeDialog = function() {
+                $mdDialog.hide();
+            };
+        }
 
 
-        // use $http or $resource
-        // https://api.punkapi.com/v2/beers
-
-        
     })
     .config(function($mdThemingProvider) {
         $mdThemingProvider.theme('docs-dark', 'default')
